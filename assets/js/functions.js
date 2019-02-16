34af6c9e6fcaa4c06c911ebc806c7f718ae0390c
javelaptop <jventura1097@gmail.com>
2019-02-15 18:31:35 -0800

    removed horizontal scroll, added horizontal scroll checker

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+    /*
+    console.log("-----------------------------");
+    console.log("Horizontal Scroll Checker!");
+    console.log("-----------------------------");
+    var docWidth = document.documentElement.offsetWidth;
+    [].forEach.call(
+        document.querySelectorAll('*'),
+        function (el) {
+            if (el.offsetWidth > docWidth) {
+                console.log(el);
+            }
+        }
+    );
+    console.log("-----------------------------");
+    */
+
     // Closes the sidebar menu
     $("#menu-close").click(function (e) {
         e.preventDefault();
@@ -10,20 +27,20 @@ $(document).ready(function () {
         $("#sidebar-wrapper").toggleClass("active");
     });
     // Scrolls to the selected menu item on the page
-    // $(function () {
-    //     $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function () {
-    //         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-    //             var target = $(this.hash);
-    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //             if (target.length) {
-    //                 $('html,body').animate({
-    //                     scrollTop: target.offset().top
-    //                 }, 2000);
-    //                 return false;
-    //             }
-    //         }
-    //     });
-    // });
+    $(function () {
+        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
     //#to-top button appears after scrolling
     var fixed = false;
@@ -50,5 +67,24 @@ $(document).ready(function () {
             }
         }
     });
-    
+    // Disable Google Maps scrolling
+    // See http://stackoverflow.com/a/25904582/1607849
+    // Disable scroll zooming and bind back the click event
+    var onMapMouseleaveHandler = function (event) {
+        var that = $(this);
+        that.on('click', onMapClickHandler);
+        that.off('mouseleave', onMapMouseleaveHandler);
+        that.find('iframe').css("pointer-events", "none");
+    }
+    var onMapClickHandler = function (event) {
+        var that = $(this);
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+    // Enable map zooming with mouse scroll when the user clicks the map
+    $('.map').on('click', onMapClickHandler);
 });
